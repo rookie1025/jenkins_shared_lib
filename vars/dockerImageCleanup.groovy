0d300b9826b1a10d56b5852bdf92c716a9d553ec
Akhil <akhil@example.com>
2023-06-17 16:54:01 +0530

    fixed type in docker cleanup

--- vars/dockerImageCleanup.groovy
+++ vars/dockerImageCleanup.groovy
@@ -2,7 +2,7 @@ def call(String project, String imageTag, String hubUser){
      
     sh """
     docker image rmi ${hubUser}/${project}:${imageTag}
-    docker image tag  ${hubUser}/${project}:latest
+    docker image rmi  ${hubUser}/${project}:latest
     """
 
 }
