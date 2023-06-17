580e45254de5a7c2220a8844e8dd9e87be61a274
Akhil <akhil@example.com>
2023-06-17 19:20:38 +0530

    new way to get credential in ecr push

--- vars/ecrImagePush.groovy
+++ vars/ecrImagePush.groovy
@@ -2,7 +2,7 @@ def call(String aws_account_id, String region, String ecr_repoName){
     
     sh """
     
-    docker login --username AWS --password $(aws ecr get-login-password --region ${region} ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+    docker login --username AWS --password $(aws ecr get-login-password --region ${region}) ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest 
     """
 
